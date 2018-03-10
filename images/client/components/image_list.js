17224dbf2a9000ccf4033699c25cd2c3d948d76e
Maksymilian Olszewski <olszewski.maksymilian@gmail.com>
2018-03-10 12:05:45 +0200

    added static data for iteration

--- images/client/components/image_list.js
+++ images/client/components/image_list.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ImageDetail from './image_detail';
 
+const IMAGES = [
+  { title: 'One', link: 'https://dummyimage.com/600x400' },
+  { title: 'Two', link: 'https://dummyimage.com/600x400' },
+  { title: 'Three', link: 'https://dummyimage.com/600x400' }
+];
+
 const ImageList = () => {
   return (
     <ul>
